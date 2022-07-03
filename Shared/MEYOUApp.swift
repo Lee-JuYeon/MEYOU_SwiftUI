4aeafbb146a77db644fc865fa280791acbe9283a
cavss <cavss1118@gmail.com>
2022-07-04 02:49:34 +0900

    Initial Commit

---
+++ Shared/MEYOUApp.swift
@@ -0,0 +1,17 @@
+//
+//  MEYOUApp.swift
+//  Shared
+//
+//  Created by C.A.V.S.S on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct MEYOUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
